598afed81bd57402d78491f8a5341ad70763ea62
Edreinne <edreinnesumabat@gmail.com>
2025-02-04 17:43:09 +0800

    Working Login

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -31,7 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
     'blog',
+    'accounts',
+
+    # Django Defaults..
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_REDIRECT_URL = 'blog:index'
